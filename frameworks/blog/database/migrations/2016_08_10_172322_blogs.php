a95af9527cdbf764c6564d57603817bf476e83a0
sxnjorombe <sxnjorombe@wichita.edu>
2016-08-11 11:36:17 -0500

    Laravel blog

---
+++ frameworks/blog/database/migrations/2016_08_10_172322_blogs.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Blogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->string('title')->index();
+            $table->string('body')->index();
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('blogs');
+    }
+}
